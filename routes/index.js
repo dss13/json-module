e4fdeb0d807150bce391988ab75c62128df9563e
dss13 <ssumugan007@gmail.com>
2017-07-13 12:06:39 +0530

    fixed 500 error

--- routes/index.js
+++ routes/index.js
@@ -78,17 +78,20 @@ router.get('/addmod', function(req, res, next) {
 })
 
 router.post('/cretect', function(req, res, next) {
-	console.log(req.body.category)
+	console.log(req.body)
 	var obj = JSON.parse(fs.readFileSync('./public/json/model.json', 'utf8'))
 	var path = './public/' + req.body.category
-	obj[req.body.category] = req.body.info
+	obj[req.body.category] = JSON.parse(req.body.info)
+	console.log(obj)
 	if (!fs.existsSync(path)) {
 	    fs.writeFile('./public/json/model.json', JSON.stringify(obj, null, 4), function(err) {
 		    if(err) {
 			    console.log(err)
 		    } else {
 		        fs.mkdirSync(path)
-			    res.render('createcat', {title: 'Add the fields for a module', message: 'category created'})
+		        // res.redirect('/createcat')
+		        // location.reload()
+			    // res.render('createcat', {title: 'Add the fields for a module', message: 'category created'})
 		    }
 	    })
 	} else {
